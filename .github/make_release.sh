aa01ea2ce9d7964c165762acf7d36ee0f27b6cde
Daniel Scheffler <danschef@gfz-potsdam.de>
2020-04-09 01:01:46 +0200

    Use make_release.sh.
    Signed-off-by: Daniel Scheffler <danschef@gfz-potsdam.de>

--- .github/make_release.sh
+++ .github/make_release.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
+USER=danschef
+REPO=doi_testproject
+URL_RELEASES=https://api.github.com/repos/$USER/$REPO/releases
+
+# create the release
 API_JSON=$(printf '{"tag_name":"%s",
                     "target_commitish":"master",
                     "name":"%s",
-                    "body":"sample message",
+                    "body":"",
                     "draft":false,
-                    "prerelease":false}' \
-    $CI_COMMIT_TAG $CI_COMMIT_TAG)
+                    "prerelease":false}' $CI_COMMIT_TAG $CI_COMMIT_TAG)
 AUTH_HEADER="Authorization: token $GITHUB_RELEASE_TOKEN"
-curl https://api.github.com/repos/danschef/doi_testproject/releases --data "$API_JSON" -H "$AUTH_HEADER"
-# curl --data "$API_JSON" \
-#     -H 'Authorization: token $GITHUB_RELEASE_TOKEN' \
-#     https://api.github.com/repos/danschef/doi_testproject/releases
-# curl https://api.github.com/repos/danschef/doi_testproject/releases --data "$API_JSON" -H "Authorization: token $GITHUB_RELEASE_TOKEN"
+curl $URL_RELEASES --data "$API_JSON" -H "$AUTH_HEADER"
+
+# get latest release tag name
+LATEST_RELEASE=$(curl -s "$URL_RELEASES" | grep -oP '"tag_name": "\K(.*)(?=")')
+
+if [ $LATEST_RELEASE != $CI_COMMIT_TAG ]; then
+ echo "Error: Could not properly create the release!" 1>&2
+ exit 1
+fi
